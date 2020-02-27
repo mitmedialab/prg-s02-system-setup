e83ceb35bd4227188909c2343151f56dd1e57e27
dev nuc <prg@s02-n00-nuc-02>
2020-02-27 15:12:03 -0500

    now can deal with docker relaunch and container ID changes

--- s02-docker_monitor.sh
+++ s02-docker_monitor.sh
@@ -4,7 +4,10 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 launch_log_window(){
     while :
     do
-          xterm -geometry 65x20+$3+$4 -T $1 -e bash -c "docker logs -f $2"
+	dID=`docker ps -qf "name=^$1" 2>/dev/null`
+	dName=`echo $1 | cut -f2 -d"_"`
+        xterm -geometry 65x20+$2+$3 -T $dName -e bash -c "docker logs -f $dID"
+	sleep 1s
     done
 }
 
@@ -16,9 +19,10 @@ let x=75; let y=0
 
 for id in $ids
 do
-    name=`docker inspect --format="{{.Name}}" $id | cut -f1 -d"." | cut -f2 -d"_"`
+    name=`docker inspect --format="{{.Name}}" $id | cut -f1 -d"."`
+    #name=`docker inspect --format="{{.Name}}" $id | cut -f1 -d"." | cut -f2 -d"_"`
     echo $id $name
-    launch_log_window $name $id $x $y&
+    launch_log_window $name $x $y&
     if [[ $x -gt 800 ]]; then
         let x=75; let y=$y+350
     else
@@ -27,4 +31,4 @@ do
 done
 
 echo "Ctrl-C to quit"
-read year
+read y
