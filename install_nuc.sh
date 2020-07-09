604306ed047436b73f460ab0678945a711329942
Hae Won Park <haewon@mit.edu>
2020-07-09 03:55:32 -0400

    option to select location

--- install_nuc.sh
+++ install_nuc.sh
@@ -9,17 +9,17 @@ line="$USER ALL=(ALL) NOPASSWD: ALL"
 sudo grep -qxF "$line" /etc/sudoers || sudo sed -i '27i '"$line"'' /etc/sudoers  &&
 echo "OK"
 
-SCHOOL="1"
-HOME="2"
+LOC_SCHOOL="1"
+LOC_HOME="2"
 echo "Select from the following option:"
 echo "[1] School Deployment"
 echo "[2] Home Deployment"
 read LOCATION
 
-if [ "$LOCATION" = "$SCHOOL" ]; then
+if [ "$LOCATION" = "$LOC_SCHOOL" ]; then
    SWARM_TOKEN="SWMTKN-1-4qtr77cbney2t4f81rj7qlz61fq78l4wyv3infn4d5lz1ct1g1-4ryfnlja84ovm2da412rk0xe2 18.27.79.165:2377"
    SHEET_URL="https://docs.google.com/spreadsheets/d/1LyPBXvrFj7XT9vVZTdyXslW371ttndbsb-SnG8kh-2M/edit?usp=sharing"
-elif [ "$LOCATION" = "$HOME" ]; then
+elif [ "$LOCATION" = "$LOC_HOME" ]; then
    SWARM_TOKEN="SWMTKN-1-3r1kuhl9wrgka8pce4slulizgo0elx8m81plxsxct31md3tbgd-by06dq1top1x9wfm3mxwn6z5b 18.27.78.195:2377" 
    SHEET_URL="https://docs.google.com/spreadsheets/d/135a5wF63Tt_AUOSR7NvoM1jeysmoccADIUxwsIcF2c0/edit?usp=sharing"
 else
@@ -37,7 +37,7 @@ fi
 echo
 echo -e "${G}Setup Jibo Audio Streaming Script${N}"
 sudo cp -R -u -p src/jibo-audio-streaming-receiver.sh  /usr/local/bin/
-if [ "$LOCATION" = "$SCHOOL" ]; then
+if [ "$LOCATION" = "$LOC_SCHOOL" ]; then
    line="/usr/local/bin/jibo-audio-streaming-receiver.sh &"
    sudo grep -qxF "$line" /etc/rc.local || sudo sed -i '$i \'"$line"'\n' /etc/rc.local
 fi
