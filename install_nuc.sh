254fcdc62356a355785ceee7b009fe4fb1777f75
Hae Won Park <haewon@mit.edu>
2020-12-02 02:12:55 -0500

    add wifi dongle configuration

--- install_nuc.sh
+++ install_nuc.sh
@@ -81,7 +81,7 @@ sudo apt-get update &&
 
 echo
 echo -e "${G}install packages${N}"
-sudo apt-get -y install vim-gnome apt-transport-https ca-certificates curl gnupg-agent software-properties-common xclip &&
+sudo apt-get -y install vim-gnome apt-transport-https ca-certificates curl gnupg-agent software-properties-common xclip hostapd haveged dnsmasq &&
 
 #echo
 #echo -e "${G}Install packages${N}"
@@ -161,6 +161,104 @@ echo -e "${G}Add Docker Monitor Script${N}"
 sudo cp -R -u -p src/s02-docker_monitor /usr/local/bin/ &&
 sudo cp -R -u -p src/docker-ros /usr/local/bin/ &&
 echo "OK"
+
+INSTALL_WIFI_DONGLE=true
+while true; do
+    read -p "Are you installing a WiFi dongle? [Y/n]: " yn
+    case $yn in
+        [Nn]* ) INSTALL_WIFI_DONGLE=false;;
+        [Yy]* ) read -n 1 -r -s -p  "Okay, setting up WiFi Dongle. Connect WiFi USB Dongle and hit enter...";;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+read -r -d '' wifi_interfaces_config <<EOF
+iface wlp0s20f3 inet static
+address 10.99.0.1
+netmask 255.255.255.0
+gateway 10.99.0.1
+EOF
+
+read -r -d '' wifi_sysctl_config <<EOF
+net.ipv4.ip_forward=1
+net.ipv6.conf.all.forwarding=1
+EOF
+
+read -r -d '' wifi_hostapd_config <<EOF
+country_code=US
+interface=wlp0s20f3
+driver=nl80211
+ssid=$HOSTNAME
+hw_mode=g
+channel=6
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=3
+wpa_passphrase=JiboHasWiFi1
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+EOF
+
+read -r -d '' wifi_dnsmasq_config <<EOF
+interface=wlp0s20f3
+dhcp-range=10.99.0.2,10.99.0.20,255.255.255.0,24h
+no-hosts
+addn-hosts=/etc/hosts.dnsmasq
+domain=wlan
+address=/gw.wlan/10.99.0.1
+EOF
+
+read -r -d '' wifi_rclocal_config <<EOF
+interface=`ip address | egrep "^[0-9]+: " | cut -d: -f2 | egrep "wlx............"`
+iptables -t nat -A POSTROUTING -o $interface -j MASQUERADE
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlp0s20f3 -o $interface -j ACCEPT
+
+iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlp0s20f3 -o eno1 -j ACCEPT
+EOF
+
+if $INSTALL_WIFI_DONGLE; then
+   if [ ! -d "/usr/local/jibo-station-wifi-service" ]; then
+
+      echo "$wifi_interfaces_config" | sudo tee -a /etc/network/interfaces
+      sudo sed -i~ 's/auto lo/auto lo wlp0s20f3/' /etc/network/interfaces
+
+      # cat /etc/network/interfaces
+      # echo
+      # echo "/etc/network/interfaces should look like this:"
+      # echo "auto lo wlp0s20f3
+      #  iface lo inet loopback
+      #  iface wlp0s20f3 inet static
+      #  address 10.99.0.1
+      #  netmask 255.255.255.0
+      #  gateway 10.99.0.1"
+
+      echo "$wifi_sysctl_config" | sudo tee -a /etc/sysctl.conf
+
+      echo "$wifi_hostapd_config" | sudo tee /etc/hostapd/hostapd.conf
+
+      echo "DAEMON_CONF=/etc/hostapd/hostapd.conf" | sudo tee -a /etc/default/hostapd
+
+      sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.dist &&
+      echo "$wifi_dnsmasq_config" | sudo tee /etc/dnsmasq.conf &&
+      sudo touch /etc/hosts.dnsmasq
+
+      sudo sed -i~ '/^Requires=network.target/a After=network-online.target\nWants=network-online.target' /lib/systemd/system/dnsmasq.service
+
+      sudo sed -i~ '/^exit 0.*/e cat wifi_rclocal_config.txt' /etc/rc.local
+
+      sudo git clone https://github.com/mitmedialab/jibo-station-wifi-service /usr/local/jibo-station-wifi-service
+      sudo chown prg /usr/local/jibo-station-wifi-service
+      cd /usr/local/jibo-station-wifi-service && ./install.sh
+   fi
+fi
+
+
+
 
 echo
 echo -e "${G}Install and Log-in to RemotePC${N}"
