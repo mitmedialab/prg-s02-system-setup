6efd58360c7cb3c9667d941a6df61389e156499d
Hae Won Park <haewon@mit.edu>
2020-12-16 11:45:36 -0500

    add switch docker swarm script

---
+++ switch_swarm.sh
@@ -0,0 +1,19 @@
+LOC_SCHOOL="1"
+LOC_HOME="2"
+echo "Select from the following option:"
+echo "[1] School Deployment"
+echo "[2] Home Deployment"
+read LOCATION
+
+if [ "$LOCATION" = "$LOC_SCHOOL" ]; then
+   SWARM_TOKEN="SWMTKN-1-4qtr77cbney2t4f81rj7qlz61fq78l4wyv3infn4d5lz1ct1g1-4ryfnlja84ovm2da412rk0xe2 18.27.79.165:2377"
+   SHEET_URL="https://docs.google.com/spreadsheets/d/1LyPBXvrFj7XT9vVZTdyXslW371ttndbsb-SnG8kh-2M/edit?usp=sharing"
+elif [ "$LOCATION" = "$LOC_HOME" ]; then
+   SWARM_TOKEN="SWMTKN-1-3r1kuhl9wrgka8pce4slulizgo0elx8m81plxsxct31md3tbgd-by06dq1top1x9wfm3mxwn6z5b 18.27.78.195:2377" 
+   SHEET_URL="https://docs.google.com/spreadsheets/d/135a5wF63Tt_AUOSR7NvoM1jeysmoccADIUxwsIcF2c0/edit?usp=sharing"
+else
+   exit
+fi
+
+
+sudo docker swarm leave && sudo docker swarm join --token $SWARM_TOKEN 
