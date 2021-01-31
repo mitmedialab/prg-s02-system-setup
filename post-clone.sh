cb6652cd274613004a05a2adb4a968b10f935159
Hae Won Park <haewon@mit.edu>
2021-01-31 04:01:45 -0500

    fix sed command; add reboot

--- post-clone.sh
+++ post-clone.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: ./post-clone.sh NEW_HOSTNAME"
+    exit
+fi
 #change hostname
-default_hn="s02-n00-nuc-110"
-target_hn="s02-n00-nuc-xxx"
+default_hn="s02-n00-nuc-xxx"
+target_hn=$1
 
-sudo sed -i 's/$default_hn/$target_hn/' /etc/hostname
-sudo sed -i 's/$default_hn/$target_hn/' /etc/hosts
-sudo sed -i 's/$default_hn/$target_hn/' /etc/hostapd/hostapd.conf
+echo $default_hn
+echo $target_hn
 
-echo 'Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot; 
+sudo sed -i~ 's/'"$default_hn"'/'"$target_hn"'/' /etc/hostname
+sudo sed -i~ 's/'"$default_hn"'/'"$target_hn"'/' /etc/hosts
+sudo sed -i~ 's/'"$default_hn"'/'"$target_hn"'/' /etc/hostapd/hostapd.conf
 
-
+echo 'Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && sudo /sbin/reboot;
