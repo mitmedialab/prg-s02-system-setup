711c34f2eb4e1f31722115665e8c3a523ab2c64a
Hae Won Park <haewon@mit.edu>
2021-01-31 03:41:44 -0500

    post clone hostname fix

---
+++ post-clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#change hostname
+default_hn="s02-n00-nuc-110"
+target_hn="s02-n00-nuc-xxx"
+
+sudo sed -i 's/$default_hn/$target_hn/' /etc/hostname
+sudo sed -i 's/$default_hn/$target_hn/' /etc/hosts
+sudo sed -i 's/$default_hn/$target_hn/' /etc/hostapd/hostapd.conf
+
+systemctl restart systemd-logind.service
+
+
