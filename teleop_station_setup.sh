12b12e23de8e44ab33e14a147e4d5c9697a23262
prg <huilichen25@gmail.com>
2021-04-28 18:03:50 -0400

    add sublime

--- teleop_station_setup.sh
+++ teleop_station_setup.sh
@@ -77,4 +77,5 @@ pip3 install pyyaml transitions
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update
 sudo apt install sublime-text
