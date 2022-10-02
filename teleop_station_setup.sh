8513c9a544a467db16542c28859d0952490eccf0
prg-family-jibo-study <prg.triadic.final.study@gmail.com>
2022-10-02 18:46:31 -0400

    add sublime

--- teleop_station_setup.sh
+++ teleop_station_setup.sh
@@ -94,10 +94,17 @@ source ~/.bashrc
 pip3 install pyyaml transitions
 
 # install sublime 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-sudo apt update
-sudo apt install sublime-text
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+# sudo apt update
+# sudo apt install sublime-text
 
 echo "cd ~/catkin_ws/src/triadic-interaction-controller" >> ~/.bashrc
+
+## SUBLIME 
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/subl
