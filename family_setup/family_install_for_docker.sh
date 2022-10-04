1a4d82d2ec3230d142079ceef74aa9098cc486de
Huili Chen <huilichen25@gmail.com>
2022-10-03 23:34:26 -0400

    Create family_install_for_docker.sh

---
+++ family_setup/family_install_for_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# secure boot should be disabled or, follow the instructions while installing this package
+sudo apt-get install -y v4l2loopback-dkms
+
+
+#setup virtual drivers module at boot
+#video virtual module 
+sudo bash -c 'cat > /etc/modules-load.d/v4l2loopback.conf <<EOL
+v4l2loopback 
+EOL'
+
+sudo bash -c 'cat >  /etc/modprobe.d/v4l2loopback.conf <<EOL
+options v4l2loopback video_nr=7 card_label="family project" 
+EOL'
+
+
+
+#audio virtual module 
+sudo bash -c 'cat >>  /etc/pulse/default.pa <<EOL
+#audio virtual module 
+load-module module-null-sink sink_name=Family
+load-module module-loopback sink=Family
+EOL'
