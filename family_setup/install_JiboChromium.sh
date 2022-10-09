edce21d7d5fb83ccecd4cdd52e0fbaa0b7dcc239
Huili Chen <huilichen25@gmail.com>
2022-10-09 16:01:21 -0400

    Update install_JiboChromium.sh

--- family_setup/install_JiboChromium.sh
+++ family_setup/install_JiboChromium.sh
@@ -4,6 +4,6 @@ if [[ ! -f "$FILE" ]]; then
    wget http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/629479/chrome-linux.zip -P ~/ 
 fi
 unzip -qq ~/chrome-linux.zip -d ~/ &&
-cp -R -u -p src/JiboChromium_logo.png ~/chrome-linux &&
-cp -R -u -p src/JiboChromium.desktop ~/Desktop &&
+cp -R -u -p ../src/JiboChromium_logo.png ~/chrome-linux &&
+cp -R -u -p ../src/JiboChromium.desktop ~/Desktop &&
 echo "OK"
