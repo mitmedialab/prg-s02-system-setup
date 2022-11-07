23a5726cc2f0f2881eda26ce41a89b992b3c145b
Huili Chen <huilichen25@gmail.com>
2022-11-06 21:25:26 -0500

    Update family_for_nuc10.sh

--- family_setup/family_for_nuc10.sh
+++ family_setup/family_for_nuc10.sh
@@ -1,8 +1,5 @@
 echo "load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket" >>  /etc/pulse/default.pa
-echo "default-server = unix:/tmp/pulseaudio.socket \\
- autospawn = no  \\
-daemon-binary = /bin/true  \\
-enable-shm = false" >>  /etc/pulse/client.conf
+echo -e "default-server = unix:/tmp/pulseaudio.socket \nautospawn = no \ndaemon-binary = /bin/true \nenable-shm = false" >>  /etc/pulse/client.conf
 
 echo "options v4l2loopback video_nr=7,8 card_label=\"family project\",\"family project 2\"" > /etc/modprobe.d/v4l2loopback.conf
 
