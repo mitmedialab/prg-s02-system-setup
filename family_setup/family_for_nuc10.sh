d1580dc2e6cbcee4e18b2ee679fea1189b69d6ee
Huili Chen <huilichen25@gmail.com>
2022-11-13 17:37:10 -0500

    Update family_for_nuc10.sh

--- family_setup/family_for_nuc10.sh
+++ family_setup/family_for_nuc10.sh
@@ -1,3 +1,7 @@
+sed -i 's/ros-bundle:triadic-interaction/ros-bundle:triadic-interaction-nuc10/g' /home/prg/prg-s02-system-setup/src/triadic-interaction
+sed -i 's/ros-bundle:triadic-interaction/ros-bundle:triadic-interaction-nuc10/g' /home/prg/prg-s02-system-setup/src/dyadic-interaction
+
+
 echo "load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket" >>  /etc/pulse/default.pa
 echo -e "default-server = unix:/tmp/pulseaudio.socket \nautospawn = no \ndaemon-binary = /bin/true \nenable-shm = false" >>  /etc/pulse/client.conf
 
