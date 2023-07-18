1cbe9541b3962c54f7b8af167e277e19c31cafde
Jon Ferguson <jon@jef.us>
2023-07-18 15:31:10 -0400

    undid the chromium install changes i did

--- s06-empathy-interactions/install_nuc_s06.sh
+++ s06-empathy-interactions/install_nuc_s06.sh
@@ -88,11 +88,11 @@ echo
 echo -e "${G}Install low version Chromium for Jibo Console${N}"
 FILE="/home/prg/chrome-linux.zip"
 if [[ ! -f "$FILE" ]]; then
-   wget http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/629479/chrome-linux.zip -P ~/ 
-   unzip -qq ~/chrome-linux.zip -d ~/ &&
-   cp -R -u -p s06-empathy-interactions/JiboChromium_logo.png ~/chrome-linux &&
-   cp -R -u -p s06-empathy-interactions/JiboChromium.desktop ~/Desktop &&
+   wget http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/629479/chrome-linux.zip -P ~/
 fi
+unzip -qq ~/chrome-linux.zip -d ~/ &&
+cp -R -u -p s06-empathy-interactions/JiboChromium_logo.png ~/chrome-linux &&
+cp -R -u -p s06-empathy-interactions/JiboChromium.desktop ~/Desktop &&
 echo "OK"
 
 echo
