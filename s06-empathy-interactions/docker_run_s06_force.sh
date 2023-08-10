3b1ea90bcdd064a8fc48e97f47df3d4eeb06aaa8
Audrey Lee <audrey16@mit.edu>
2023-08-10 11:54:43 -0400

    Fix docker run scripts

--- s06-empathy-interactions/docker_run_s06_force.sh
+++ s06-empathy-interactions/docker_run_s06_force.sh
@@ -21,10 +21,10 @@ docker stop s06-microphone s06-camera
 docker container prune --force
 
 echo -e "Running microphone container"
-docker run --env-file docker-compose.env -d -it --name=s06-microphone --label="device_container" --ipc="host" --device=/dev/snd -v /etc/asound.conf:/etc/asound.conf --restart=always --network=host docker-registry.jibo.media.mit.edu:5000/s06-ros /bin/bash -c "sudo /etc/init.d/alsa-utils restart; sudo chown -R :prg /dev; cd ../asr_assembly/src; python3.8 local_mic_asr.py"
+docker run --env-file docker-compose.env -d -it --name=s06-microphone --label="device_container" --ipc="host" --device=/dev/snd -v /etc/asound.conf:/etc/asound.conf --restart=always --network=host docker-registry.jibo.media.mit.edu:5000/s06-ros:latest /bin/bash -c "sudo /etc/init.d/alsa-utils restart; sudo chown -R :prg /dev; cd ../asr_assembly/src; python3.8 local_mic_asr.py"
 
 echo -e "Running USB camera container"
-docker run --env-file docker-compose.env -d -it --name=s06-camera --label="device_container" --ipc="host" --device=/dev/snd --device=/dev/video0 -v /etc/asound.conf:/etc/asound.conf -v /home/prg/s06-empathy-interaction/empathy_videos:/home/prg/s06-empathy-interaction/empathy_videos --restart=always --network=host docker-registry.jibo.media.mit.edu:5000/s06-ros /bin/bash -c "sudo /etc/init.d/alsa-utils restart; sudo chown -R :prg /dev; sudo chown -R :prg /home/prg/s06-empathy-interaction; cd ../usb_cam; python3.8 usb_cam_cmd.py"
+docker run --env-file docker-compose.env -d -it --name=s06-camera --label="device_container" --ipc="host" --device=/dev/snd --device=/dev/video0 -v /etc/asound.conf:/etc/asound.conf -v /home/prg/s06-empathy-interaction/empathy_videos:/home/prg/s06-empathy-interaction/empathy_videos --restart=always --network=host docker-registry.jibo.media.mit.edu:5000/s06-ros:latest /bin/bash -c "sudo /etc/init.d/alsa-utils restart; sudo chown -R :prg /dev; sudo chown -R :prg /home/prg/s06-empathy-interaction; cd ../usb_cam; python3.8 usb_cam_cmd.py"
 
 if ! egrep -q docker_run_s06.sh /etc/rc.local; then
     if [[ $(tail -1 /etc/rc.local) = "exit 0" ]]; then
