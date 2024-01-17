196333e7b278c7f54d1adc7c04fd43d642ef94e9
Jon Ferguson <jon@jef.us>
2024-01-17 12:16:16 -0500

    updated buildroot's docker swarm token

--- switch_swarm.sh
+++ switch_swarm.sh
@@ -2,13 +2,14 @@ LOC_SCHOOL="1"
 LOC_HOME="2"
 LOC_DEV="3"
 echo "Select from the following option:"
-echo "[1] School Deployment"
-echo "[2] Home Deployment"
-echo "[3] DEV Deployment"
+echo "[1] School Deployment (buildroot.media.mit.edu) (recommended)"
+echo "[2] Home Deployment (prg-webhost.media.mit.edu)"
+#echo "[3] DEV Deployment"  # dead
 read LOCATION
 
 if [ "$LOCATION" = "$LOC_SCHOOL" ]; then
-   SWARM_TOKEN="SWMTKN-1-4qtr77cbney2t4f81rj7qlz61fq78l4wyv3infn4d5lz1ct1g1-4ryfnlja84ovm2da412rk0xe2 18.27.79.165:2377"
+   #SWARM_TOKEN="SWMTKN-1-4qtr77cbney2t4f81rj7qlz61fq78l4wyv3infn4d5lz1ct1g1-4ryfnlja84ovm2da412rk0xe2 18.27.79.165:2377"
+   SWARM_TOKEN="SWMTKN-1-3zc9c3xnf20ue7mgkhu8then3lf0kdhdg27i7qk7naurrjnlvv-8k38rf1t1oiwo30lhx1rpcuq5 18.27.79.165:2377"
    SHEET_URL="https://docs.google.com/spreadsheets/d/1LyPBXvrFj7XT9vVZTdyXslW371ttndbsb-SnG8kh-2M/edit?usp=sharing"
 elif [ "$LOCATION" = "$LOC_HOME" ]; then
    SWARM_TOKEN="SWMTKN-1-3r1kuhl9wrgka8pce4slulizgo0elx8m81plxsxct31md3tbgd-by06dq1top1x9wfm3mxwn6z5b 18.27.78.195:2377" 
