eee8f9aec3bcf7a5540f499871b5391541dcb4d8
Jon Ferguson <jon@media.mit.edu>
2025-03-21 10:35:12 -0400

    bind mount /media instead of /media/prg since /media/prg might not exists on machine that have never had removable media mounted

--- setup_data-tardis.sh
+++ setup_data-tardis.sh
@@ -1,4 +1,4 @@
-CONTAINER_NAME="prg-data-tardis"
+CONTAINER_NAME="data-tardis"
 IMAGE_NAME="docker-registry.jibo.media.mit.edu:5000/mitprg/prg-data-tardis:20250321"
 
 while true; do
@@ -13,5 +13,5 @@ done
 if $INSTALL_DATA_TARDIS_CONTAINER; then
     docker rm -f $CONTAINER_NAME
     docker pull "$IMAGE_NAME"
-    docker run -d --restart=unless-stopped --name="$CONTAINER_NAME" -p 1963:1963 --privileged -v /dev:/dev --env ETCO_synology_password="9^jvH%5cU6#E" --env HOST_HOSTNAME="`hostname`" --env ETCO_delete_after_upload=1 --mount type=bind,source=/media/prg,target=/media/prg,bind-propagation=shared "$IMAGE_NAME"
+    docker run -d --restart=unless-stopped --name="$CONTAINER_NAME" -p 1963:1963 --privileged -v /dev:/dev --env ETCO_synology_password="9^jvH%5cU6#E" --env HOST_HOSTNAME="`hostname`" --env ETCO_delete_after_upload=1 --mount type=bind,source=/media,target=/media,bind-propagation=shared "$IMAGE_NAME"
 fi
