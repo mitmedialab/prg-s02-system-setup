09e9b617e74184876e6a4fc96fd9f2545550fec2
Jon Ferguson <jon@media.mit.edu>
2025-03-21 08:50:43 -0400

    added setup_data-tardis.sh script

---
+++ setup_data-tardis.sh
@@ -0,0 +1,17 @@
+CONTAINER_NAME="prg-data-tardis"
+IMAGE_NAME="docker-registry.jibo.media.mit.edu:5000/mitprg/prg-data-tardis:20250321"
+
+while true; do
+    read -p "Proceed with setting up data-tardis docker container? [Y/n]: " yn
+    case $yn in
+        [Nn]* ) INSTALL=false; echo "Please run ./setup_data-tardis.sh later"; break;;
+        [Yy]*|"" ) echo "Okay, setting up data-tardis docker container."; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+if $INSTALL_DATA_TARDIS_CONTAINER; then
+    docker rm -f $CONTAINER_NAME
+    docker pull "$IMAGE_NAME"
+    docker run -d --restart=unless-stopped --name="$CONTAINER_NAME" -p 1963:1963 --privileged -v /dev:/dev --env ETCO_synology_password="9^jvH%5cU6#E" --env HOST_HOSTNAME="`hostname`" --env ETCO_delete_after_upload=1 --mount type=bind,source=/media/prg,target=/media/prg,bind-propagation=shared "$IMAGE_NAME"
+fi
