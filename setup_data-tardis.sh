700a041c603cfd66d96b32342ca55122652fe189
Jon Ferguson <jon@jef.us>
2025-03-25 10:52:48 -0400

    having the data-taris directory mounted inside the container is pretty important

--- setup_data-tardis.sh
+++ setup_data-tardis.sh
@@ -13,5 +13,5 @@ done
 if $INSTALL_DATA_TARDIS_CONTAINER; then
     docker rm -f $CONTAINER_NAME
     docker pull "$IMAGE_NAME"
-    docker run -d --restart=unless-stopped --name="$CONTAINER_NAME" -p 1963:1963 --privileged -v /dev:/dev --env ETCO_synology_password="9^jvH%5cU6#E" --env HOST_HOSTNAME="`hostname`" --env ETCO_delete_after_upload=1 --mount type=bind,source=/media,target=/media,bind-propagation=shared "$IMAGE_NAME"
+    docker run -d --restart=unless-stopped --name="$CONTAINER_NAME" -p 1963:1963 --privileged -v /home/prg/rosbags/data-tardis:/data/tardis -v /dev:/dev --env ETCO_synology_password="9^jvH%5cU6#E" --env HOST_HOSTNAME="`hostname`" --env ETCO_delete_after_upload=1 --mount type=bind,source=/media,target=/media,bind-propagation=shared "$IMAGE_NAME"
 fi
