2105beb234b70036bec37a98ebfc15ba9d3fb7c1
Jon Ferguson <jon@jef.us>
2025-03-27 15:15:27 -0400

    changed override to INSTALL_DATA_TARDIS_CONTAINER and fixed bug

--- setup_data-tardis.sh
+++ setup_data-tardis.sh
@@ -1,18 +1,18 @@
 CONTAINER_NAME="data-tardis"
 IMAGE_NAME="docker-registry.jibo.media.mit.edu:5000/mitprg/prg-data-tardis:20250327"
 
-if [[ -z $NO_PROMPTING ]]; then
+if [[ -z $INSTALL_DATA_TARDIS_CONTAINER ]]; then
     while true; do
         read -p "Proceed with setting up data-tardis docker container? [Y/n]: " yn
         case $yn in
-            [Nn]* ) INSTALL=false; echo "Please run ./setup_data-tardis.sh later"; break;;
-            [Yy]*|"" ) echo "Okay, setting up data-tardis docker container."; break;;
+            [Nn]* ) echo "Please run ./setup_data-tardis.sh later"; break;;
+            [Yy]*|"" ) INSTALL_DATA_TARDIS_CONTAINER=1; echo "Okay, setting up data-tardis docker container."; break;;
             * ) echo "Please answer yes or no.";;
         esac
     done
 fi
     
-if $INSTALL_DATA_TARDIS_CONTAINER; then
+if [[ ! -z $INSTALL_DATA_TARDIS_CONTAINER ]]; then
     docker rm -f $CONTAINER_NAME
     docker pull "$IMAGE_NAME"
     docker run -d --restart=unless-stopped --name="$CONTAINER_NAME" -p 1963:1963 --privileged -v /home/prg/rosbags/data-tardis:/data/tardis -v /dev:/dev --env ETCO_synology_password="9^jvH%5cU6#E" --env HOST_HOSTNAME="`hostname`" --env ETCO_delete_after_upload=1 --mount type=bind,source=/media,target=/media,bind-propagation=shared "$IMAGE_NAME"
